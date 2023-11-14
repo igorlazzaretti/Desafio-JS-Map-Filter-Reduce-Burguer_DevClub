5ebf66627e0009088a65746189d39e8f39bafffe
Igor Dossin Lazzaretti <idlazzaretti@gmail.com>
2023-11-14 11:50:14 -0300

    Acredite nos seus sonhos

--- products.js
+++ products.js
@@ -1,5 +1,5 @@
 const menuOptions = [
-    { name: 'X-Salada', price: 30.00, vegan: false, src: './assets/xsalada.jpeg' },
+    { name: 'X-Salada', price: 30, vegan: false, src: './assets/xsalada.jpeg' },
     { name: 'X-Bacon', price: 34, vegan: false, src: './assets/xbacon.png' },
     { name: 'X-Bacon Egg', price: 39, vegan: false, src: './assets/bacon-egg.png' },
     { name: 'Monstruoso', price: 50, vegan: false, src: './assets/monstruoso.png' },
---
+++ script.js
@@ -0,0 +1,68 @@
+
+const varUl = document.querySelector('ul')
+
+
+const buttonforEach = document.querySelector('#forEach-button')
+const buttonMap     = document.querySelector('#map-button')
+
+
+function showAll(array){
+        let varMyLi = ''
+        array.forEach( (products) => {
+        varMyLi += 
+     `
+     <li class="list">
+      <img src=${products.src} alt="Foto do Hamburguer">
+    <p>${products.name}</p>
+    <p class="item-price">R$ ${products.price}</p>
+    </li>
+    `
+    
+});
+
+varUl.innerHTML = varMyLi
+}
+
+
+
+function showForEach(){
+
+    showAll(menuOptions) 
+
+}
+
+
+function showMap(){
+
+    const newPricesMap = menuOptions.map((products) => ({
+
+        ...products,
+        price:products.price * 0.9,
+    }))
+
+    showAll(newPricesMap)
+
+}
+
+function showReduce (){
+
+
+
+}
+
+function showFilter (){
+
+
+
+}
+
+
+
+buttonforEach.addEventListener("click", showForEach)
+buttonMap.addEventListener("click", showMap)
+buttonReduce.addEventListener("click", showReduce)
+buttonFilter.addEventListener("click", showFilter)
+
+
+
+
